32d157287dca2eccb00bfeb900ad5176f74306ed
medeirosjorge <medeirosjorge__@hotmail.com>
2023-12-01 12:21:05 -0300

    Remove Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,13 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@argon.com',
-            'password' => '$2y$10$eMMXLkP579E/hf8.oSBJRu.yndQDIU0XrjRsY/R9Sr6hxzjToy0gC',
-            'nivel'=>'ADMIN',
-        ]);
-    }
+    // public function run()
+    // {
+    //     DB::table('users')->insert([
+    //         'name' => 'admin',
+    //         'email' => 'admin@argon.com',
+    //         'password' => '$2y$10$eMMXLkP579E/hf8.oSBJRu.yndQDIU0XrjRsY/R9Sr6hxzjToy0gC',
+    //         'nivel'=>'ADMIN',
+    //     ]);
+    // }
 }
