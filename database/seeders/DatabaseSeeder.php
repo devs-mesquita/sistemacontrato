e30fc991f28923c7f96fa9a2250039f835c4608a
medeirosjorge <medeirosjorge__@hotmail.com>
2023-12-01 12:23:25 -0300

    Tirei o db:seed do scripts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,18 +8,13 @@ use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    // public function run()
-    // {
-    //     DB::table('users')->insert([
-    //         'name' => 'admin',
-    //         'email' => 'admin@argon.com',
-    //         'password' => '$2y$10$eMMXLkP579E/hf8.oSBJRu.yndQDIU0XrjRsY/R9Sr6hxzjToy0gC',
-    //         'nivel'=>'ADMIN',
-    //     ]);
-    // }
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@argon.com',
+            'password' => '$2y$10$eMMXLkP579E/hf8.oSBJRu.yndQDIU0XrjRsY/R9Sr6hxzjToy0gC',
+            'nivel'=>'ADMIN',
+        ]);
+    }
 }
--- scripts.sh
+++ scripts.sh
@@ -3,5 +3,5 @@ php artisan key:generate && \
 php artisan config:cache &&\
 php artisan route:cache &&\
 php artisan migrate --force && \
-php artisan db:seed --force && \
+# php artisan db:seed --force && \
 apache2ctl -D FOREGROUND
