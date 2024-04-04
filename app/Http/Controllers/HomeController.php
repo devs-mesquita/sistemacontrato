3443e8386f82a58a368676b1c3e39275e7f550c1
Ismael Lima <79029743+L3EWick@users.noreply.github.com>
2024-04-04 13:43:17 -0300

    dashboard adicionado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key;
 
 class HomeController extends Controller
 {
-        /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\View\View
-     */
     public function index()
     {
-        return view('pages.dashboard');
+        $metabaseSiteUrl = 'https://metabase.mesquita.rj.gov.br';
+        $metabaseSecretKey = env('METABASE_SECRET_KEY', '');
+        
+        $signer = new Sha256();
+        $builder = new Builder();
+
+        $token = $builder
+            ->setIssuer($metabaseSiteUrl) 
+            ->setAudience($metabaseSiteUrl) 
+            ->setId($metabaseSecretKey, true)
+            ->setIssuedAt(time()) 
+            ->set('resource', ['dashboard' => 25])
+            ->set('params', (object)[])
+            ->sign($signer, $metabaseSecretKey) 
+            ->getToken();
+
+        $iframeUrl = "{$metabaseSiteUrl}/embed/dashboard/{$token}#bordered=true&titled=false";
+
+        return view('pages.dashboard', compact('iframeUrl'));
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,11 +1,22 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
-
+<link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" />
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Home'])
-        @include('layouts.footers.auth.footer')
-   
+    @include('layouts.navbars.auth.topnav', ['title' => 'Contratos'])
+    <div class="card">
+        <div class="card-body">
+            <iframe
+                src="{{$iframeUrl}}"
+                frameborder="0"
+                width="100%"
+                height="1000"
+                allowtransparency>
+            </iframe>
+        </div>
+    </div>
+    @include('layouts.footers.auth.footer')
 @endsection
-<script src="{{asset('assets/js/plugins/chartjs.min.js')}}"></script>
+
 @push('js')
-   
+
 @endpush
+
