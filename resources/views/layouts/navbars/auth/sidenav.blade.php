ef61d368035c490ce35c3b15ae65c5333fc60ecf
Jorge Medeiros <medeirosjorge__@hotmail.com>
2024-08-06 11:00:38 -0300

    fontawesome/correcao/responsividade

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -3,17 +3,17 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0 " style="text-align: center;" href="{{ route('home') }}"
-            >
-            <img src="{{asset("img/brasao.png")}}" class="navbar-brand-img h-100" alt="main_logo">
+        <a class="navbar-brand m-0 " style="text-align: center;" href="{{ route('home') }}">
+            <img src="{{ asset('img/brasao.png') }}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Controlador de Contratos</span>
         </a>
     </div>
-            <hr class="horizontal dark mt-0">
-            <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-            <ul class="navbar-nav">
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+        <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}" href="{{ route('home') }}">
+                <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}"
+                    href="{{ route('home') }}">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
@@ -21,9 +21,10 @@
                     <span class="nav-link-text ms-1">Inicio</span>
                 </a>
             </li>
-            
+
             <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'contrato.index' ? 'active' : '' }}" href="{{ route('contrato.index') }}">
+                <a class="nav-link {{ Route::currentRouteName() == 'contrato.index' ? 'active' : '' }}"
+                    href="{{ route('contrato.index') }}">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-folder-17 text-primary text-sm opacity-10"></i>
@@ -31,45 +32,47 @@
                     <span class="nav-link-text ms-1">Contratos</span>
                 </a>
             </li>
-            @if (Auth::user()->nivel != "USUARIO")
-            <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'responsavel.index' ? 'active' : '' }}" href="{{ route('responsavel.index') }}">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-badge text-primary text-sm opacity-10"></i>
-                    </div>
-                    
-                    <span class="nav-link-text ms-1">Responsáveis</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'setor.index' ? 'active' : '' }}" href="{{ route('setor.index') }}">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-bullet-list-67 text-primary text-sm opacity-10"></i>
-                    </div>
-                    
-                    <span class="nav-link-text ms-1">Setores</span>
-                </a>
-            </li>
-            @if (Auth::user()->nivel == 'SUPERADMIN'|| Auth::user()->nivel == 'ADMIN'||Auth::user()->nivel == 'VIEWER')
-            <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'user.index' ? 'active' : '' }}" href="{{ route('user.index') }}">
-                   
-                    <div
-                
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-single-02 text-primary text-sm opacity-10"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Usuarios</span>
-                </a>
-            </li>
-            @endif
-            
+            @if (Auth::user()->nivel != 'USUARIO')
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::currentRouteName() == 'responsavel.index' ? 'active' : '' }}"
+                        href="{{ route('responsavel.index') }}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-badge text-primary text-sm opacity-10"></i>
+                        </div>
+
+                        <span class="nav-link-text ms-1">Responsáveis</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::currentRouteName() == 'setor.index' ? 'active' : '' }}"
+                        href="{{ route('setor.index') }}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-bullet-list-67 text-primary text-sm opacity-10"></i>
+                        </div>
+
+                        <span class="nav-link-text ms-1">Setores</span>
+                    </a>
+                </li>
+                @if (Auth::user()->nivel == 'SUPERADMIN' || Auth::user()->nivel == 'ADMIN' || Auth::user()->nivel == 'VIEWER')
+                    <li class="nav-item">
+                        <a class="nav-link {{ Route::currentRouteName() == 'user.index' ? 'active' : '' }}"
+                            href="{{ route('user.index') }}">
+
+                            <div
+                                class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                                <i class="ni ni-single-02 text-primary text-sm opacity-10"></i>
+                            </div>
+                            <span class="nav-link-text ms-1">Usuarios</span>
+                        </a>
+                    </li>
+                @endif
+
         </ul>
         @endif
     </div>
-    
+
     <br>
     <br>
     <br>
@@ -80,46 +83,44 @@
     <br>
 
 
-<div>
-    <a type="button"  class="sidebar-footer hidden-small"  data-bs-toggle="modal" data-bs-target="#atualizaModal" style="text-align: center font-size: 15px; color: #bfa15f;padding-bottom:15px">
-       Notas de Atualização
-    </a> 
+    <div>
+        <a type="button" class="sidebar-footer hidden-small" data-bs-toggle="modal" data-bs-target="#atualizaModal"
+            style="text-align: center font-size: 15px; color: #bfa15f;padding-bottom:15px">
+            Notas de Atualização
+        </a>
 
-</div>
+    </div>
 
-      <div class="modal fade" id="atualizaModal" tabindex="-1" role="dialog" aria-labelledby="atualizaModalLabel" aria-hidden="true">
+    <div class="modal fade" id="atualizaModal" tabindex="-1" role="dialog" aria-labelledby="atualizaModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="atualizaModalLabel">Notas de Atualização</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="atualizaModalLabel">Notas de Atualização</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h4>Versão Beta 1.0</h4>
+                    <p> Essa versão contem as funcionalidades basicas do sistema</p>
+                    <li>Criar novos Contratos</li>
+                    <li>Trocar status do Contrato</li>
+                    <li>Criar novos Consultores</li>
+                    <p></p>
+                    <h4>Versão Beta 2.0</h4>
+                    <li>Adicionado Lista de Responsáveis</li>
+                    <li>Adicionado CPF no cadastro do Usuário</li>
+                    <li>Adicionado telefone no cadastro do Fiscal</li>
+                    <li>Adicionado novas informações na tela principal.</li>
+                    <li>Adicionado botao alterar status, com o motivo da alteração.</li>
+                    <li>Lista de criação de setores.</li>
+                    <li>Alteração de ícones barra de navegação.</li>
+                    <li>Criação de um nivel de permissão para visualização.</li>
+                </div>
             </div>
-            <div class="modal-body">
-                <h4>Versão Beta 1.0</h4>
-                <p> Essa versão contem as funcionalidades basicas do sistema</p>
-                <li>Criar novos Contratos</li>
-                <li>Trocar status do Contrato</li>
-                <li>Criar novos Consultores</li>
-                <p></p>
-                <h4>Versão Beta 2.0</h4>
-                <li>Adicionado Lista de Responsáveis</li>
-                <li>Adicionado CPF no cadastro do Usuário</li>
-                <li>Adicionado telefone no cadastro do Fiscal</li>
-                <li>Adicionado novas informações na tela principal.</li>
-                <li>Adicionado botao alterar status, com o motivo da alteração.</li>
-                <li>Lista de criação de setores.</li>
-                <li>Alteração de ícones barra de navegação.</li>
-                <li>Criação de um nivel de permissão para visualização.</li>
-            </div>
-        </div>
         </div>
     </div>
 
 
 </aside>
-
-
-
-
--- resources/views/layouts/navbars/auth/topnav.blade.php
+++ resources/views/layouts/navbars/auth/topnav.blade.php
@@ -1,40 +1,31 @@
 <!-- Navbar -->
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl
-        {{ str_contains(Request::url(), 'virtual-reality') == true ? ' mt-3 mx-3 bg-primary' : '' }}"
-    id="navbarBlur" data-scroll="false">
-    <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
-
-
-        </nav>
-        <div class="top_nav">
-            <div class="nav_menu align-right" style="
-            padding-right: 30px;
+<nav
+    class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl
+        {{-- {{ str_contains(Request::url(), 'virtual-reality') == true ? ' mt-3 mx-3 bg-primary' : '' }}" id="navbarBlur"
+        data-scroll="false --}}
         ">
-                <ul class="nav navbar-nav navbar-right">
+    <div class="container-fluid py-1 px-3">
 
-                    <div class="dropdown">
-                        <button class="btn bg-gradient-primary dropdown-toggle" type="button" id="dropdownMenuButton"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Menu
-                        </button>
-                        <ul style="top: 0rem !important;" class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <li><a style="color: black" class="dropdown-item" href="{{route('alterasenha')}}">Alterar Senha</a></li>
-                            <li>
-                                <form role="form" method="post" action="{{ route('logout') }}" id="logout-form">
-                                    @csrf
-                                    <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                                        class="nav-link text-white font-weight-bold px-0">
-                                        <i class="fa fa-user me-sm-1"></i>
-                                        <span class="d-sm-inline d-none" style="color: black">Sair</span>
-                                    </a>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
 
-                    {{-- <li class="nav-item d-flex align-items-center">
+            <ul class="ms-md-auto pe-md-3 d-flex" style="align-items: baseline;">
+
+                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                        <div class="sidenav-toggler-inner">
+                            <i class="sidenav-toggler-line bg-white"></i>
+                            <i class="sidenav-toggler-line bg-white"></i>
+                            <i class="sidenav-toggler-line bg-white"></i>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item px-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-white p-0">
+                        <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
+                    </a>
+                </li>
+
+                {{-- <li class="nav-item d-flex align-items-center">
                     <form role="form" method="post" action="{{ route('logout') }}" id="logout-form">
                         @csrf
                         <a href="{{ route('logout') }}"
@@ -43,27 +34,11 @@
                             <i class="fa fa-user me-sm-1"></i>
                             <span class="d-sm-inline d-none">Sair</span>
                         </a>
-                        <div class="collapse navbar-collapse mt-sm1 mt2 me-md2 me-sm-3">
-                        <a href="" class="nav-link text-white font-weight-bold px-0">
-                            <i class="fa fa-lock me-sm-1"></i>
-                            <span class="d-sm-inline d-none">Alterar Senha</span>
-                        </a>
-                        </div>
                     </form>
                 </li> --}}
 
-
-
-
-
-
-                    </li>
-                </ul>
-            </div>
+            </ul>
         </div>
+    </div>
 </nav>
-</div>
-
-
-
 <!-- End Navbar -->
